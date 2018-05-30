ee479e7c5a9b83f790b5c0176a7c37128c7e83ee
juliatruitt <jrt12@comcast.net>
2018-05-29 17:48:11 -0700

    Created sample card using html and css

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+        <div className="imageCard">
+          <div className="leftAligned">
+            <h1 className="imageTitle">Image Title</h1>
+            <img src={"https://www.efilecabinet.com/wp-content/uploads/2017/03/how-work-life-balance-can-be-achieved.jpg"}
+                 alt="Image"
+                 className="scaledImage"/>
+          </div>
+          <div className="rightAligned">
+            <h4>How it affects women in general</h4>
+            <h4>Personal Quotes</h4>
+          </div>
+        </div>
       </div>
     );
   }
