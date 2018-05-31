8e38523b8fa5abf14eb0bc818c7f25d356db9209
juliatruitt <jrt12@comcast.net>
2018-05-30 20:30:48 -0700

    Added first 5 cards, adjusted css accordingly

--- src/InfoCard.js
+++ src/InfoCard.js
@@ -4,21 +4,21 @@ import React from 'react';
 const InfoCard = (props) => {
     return(
         <div className="imageCard">
-        <div className="leftAligned">
-          <h1 className="imageTitle">{props.title}</h1>
-          <img src={props.imageLink}
+          <div className="leftAligned">
+            <h4 className="imageTitle">{props.title}</h4>
+            <img src={props.imageLink}
                alt="Image"
                className="scaledImage"/>
+          </div>
+          <div className="rightAligned">
+            <h5>How it affects women in general:</h5>
+            <p>{props.affectsWomen}</p>
+            <h5>Personal Quotes:</h5>
+            <p>{props.quote}</p>
+          </div>
         </div>
-        <div className="rightAligned">
-          <h4>How it affects women in general:</h4>
-          <div>{props.affectsWomen}</div> 
-          <h4>Personal Quotes</h4>
-          <div>{props.quote}</div> 
-        </div>
-      </div>
     )
 }
 
 
-export default InfoCard; 
+export default InfoCard;
