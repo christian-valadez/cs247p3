3401519a65412844154ec7f2776833108047a590
Christian Valadez <cvaladez@stanford.edu>
2018-06-06 21:52:55 -0700

    Added functionality to randomize images every time

--- src/App.js
+++ src/App.js
@@ -21,6 +21,19 @@ import update from 'immutability-helper';
 
 import _ from 'lodash';
 
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
+
+shuffle(infoCards);
+
 const bankImages = infoCards.map(item => {
   return item.imageLink; 
 })
